c37ccf210b31a6df9db57ccf480c6995cd7c1ad1
Fernando Zapata <fezami@hotmail.com>
2022-05-25 16:50:13 -0500

    terminado el model visita y usuario controller

--- src/main/java/com/example/entity/Usuario.java
+++ src/main/java/com/example/entity/Usuario.java
@@ -21,9 +21,13 @@ import org.springframework.format.annotation.DateTimeFormat;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import lombok.Getter;
+import lombok.Setter;
 
 
 
+@Getter
+@Setter
 @Entity
 @Table(name = "Usuario")
 public class Usuario {
@@ -76,6 +80,7 @@ public class Usuario {
 	@Basic(optional = false)
 	@Column(name = "fechareg")
 	private Date fechareg;
+
 	
 	@JsonIgnoreProperties({ "hibernateLazyInitializer", "handler" })
 	@ManyToOne(fetch = FetchType.LAZY)
@@ -94,121 +99,6 @@ public class Usuario {
 	@Transactional
 	public String getNombreCompleto() {
 		return nombre.concat(" ").concat(primerapellido+" "+segundoapellido);
-	}
-
-	public int getIdUsuario() {
-		return idUsuario;
-	}
-
-	public void setIdUsuario(int idUsuario) {
-		this.idUsuario = idUsuario;
-	}
-
-	public String getDni() {
-		return dni;
-	}
-
-	public void setDni(String dni) {
-		this.dni = dni;
-	}
-
-	public String getNombre() {
-		return nombre;
-	}
-
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-
-	public String getPrimerapellido() {
-		return primerapellido;
-	}
-
-	public void setPrimerapellido(String primerapellido) {
-		this.primerapellido = primerapellido;
-	}
-
-	public String getSegundoapellido() {
-		return segundoapellido;
-	}
-
-	public void setSegundoapellido(String segundoapellido) {
-		this.segundoapellido = segundoapellido;
-	}
-
-	public String getTelefono() {
-		return telefono;
-	}
-
-	public void setTelefono(String telefono) {
-		this.telefono = telefono;
-	}
-
-	public String getCelular() {
-		return celular;
-	}
-
-	public void setCelular(String celular) {
-		this.celular = celular;
-	}
-
-	public String getDireccion() {
-		return direccion;
-	}
-
-	public void setDireccion(String direccion) {
-		this.direccion = direccion;
-	}
-
-	public String getCorreo() {
-		return correo;
-	}
-
-	public void setCorreo(String correo) {
-		this.correo = correo;
-	}
-
-	public String getPassword() {
-		return password;
-	}
-
-	public void setPassword(String password) {
-		this.password = password;
-	}
-
-	public Date getFechareg() {
-		return fechareg;
-	}
-
-	public void setFechareg(Date fechareg) {
-		this.fechareg = fechareg;
-	}
-
-	public Ubigeo getIdUbigeo() {
-		return idUbigeo;
-	}
-
-	public void setIdUbigeo(Ubigeo idUbigeo) {
-		this.idUbigeo = idUbigeo;
-	}
-
-	public String getLogin() {
-		return login;
-	}
-
-	public void setLogin(String login) {
-		this.login = login;
-	}
-
-	public boolean isEstado() {
-		return estado;
-	}
-
-	public void setEstado(boolean estado) {
-		this.estado = estado;
-	}
-
-	
-	
+	}	
 	
 }
